0ae0b885eff097d1b2d2c88adfcfe361ad44ec1f
Oggi Danailov <ogidan@abv.bg>
2017-11-07 22:44:38 -0500

    navbar centered

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,20 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def noun
+  end
+
+  def verbs
+  end
+
+  def adjectives
+  end
+
+  def words
+  end
+
+  def expressions
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root 'welcome#index'
+
+get '/noun' => 'welcome#noun'
+get '/adjectives' => 'welcome#adjectives'
+get '/expressions' => 'welcome#expressions'
+get 'verbs' => 'welcome#verbs'
+get 'words' => 'welcome#words'
 end
